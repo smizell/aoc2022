61b3c25c155a0f2e5967d5641813841676199cbf
Stephen Mizell <smizell@gmail.com>
2022-12-02 23:21:30 -0600

    Day 3 Part 1

--- lib/day03.rb
+++ lib/day03.rb
@@ -1,5 +1,32 @@
+require "set"
+
 class Day03
-  def part1
+  def part1(inputs)
+    rucksacks = inputs.split("\n").map do |line|
+      [line[0, line.size / 2], line[line.size / 2..]]
+    end
+
+    priorities = rucksacks.map do |compartment1, compartment2|
+      compartment1_set = compartment1.chars.to_set
+      compartment2_set = compartment2.chars.to_set
+      item_type = compartment1_set.intersection(compartment2_set).to_a[0]
+
+      if upper?(item_type)
+        item_type.downcase.bytes[0] - 70
+      else
+        item_type.bytes[0] - 96
+      end
+    end
+
+    priorities.sum
+  end
+
+  def example_part1
+    157
+  end
+
+  def upper?(value)
+    value == value.upcase
   end
 
   def part2
